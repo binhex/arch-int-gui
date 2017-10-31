abc7d0ee28bea6136de6ed3dbdaca176c9d32aba
binhex <megalith01@gmail.com>
2017-10-31 16:52:19 +0000

    comment enhancement

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -9,7 +9,7 @@ rm -rf /tmp/.X*; vncserver :0 -depth 24 -SecurityTypes=None
 # starts novnc (web vnc client) - note the launch.sh also starts websockify to connect novnc to tigervnc server
 /usr/share/novnc/utils/launch.sh --listen 6080 --vnc localhost:5900 &
 
-# start dbus (required for libreoffice menus) and launch openbox (window manager)
+# start dbus (required for libreoffice menus to be viewable when started via openbox right click menu) and launch openbox (window manager)
 dbus-launch openbox-session &
 
 # run tint2 (creates task bar) with custom theme
