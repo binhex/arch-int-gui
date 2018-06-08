50a54d741101b48a6ee31119e79cbbdfcc1b39c6
binhex <megalith01@gmail.com>
2018-06-08 14:45:34 +0100

    allow user to specify password for web ui

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -5,8 +5,19 @@
 # create env var for display (note display number must match for tigervnc)
 export DISPLAY=:0
 
+# vnc start command
+vnc_start="rm -rf /tmp/.X*; vncserver :0 -depth 24"
+
+# if a password is specified then set generate password file
+# else append insecure flag to command line
+if [[ -n "${VNC_PASSWORD}" ]]; then
+	echo -e "${VNC_PASSWORD}\n${VNC_PASSWORD}\nn" | vncpasswd 1>&- 2>&-
+else
+	vnc_start="${vnc_start} -SecurityTypes=None"
+fi
+
 # start tigervnc (vnc server) - note the port that it runs on is 5900 + display number (i.e. 5900 + 0 in the case below).
-rm -rf /tmp/.X*; vncserver :0 -depth 24 -SecurityTypes=None
+eval "${vnc_start}"
 
 # if defined then set title for the web ui tab
 if [[ -n "${WEBPAGE_TITLE}" ]]; then
