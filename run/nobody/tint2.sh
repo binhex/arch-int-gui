5d0263ab8b66c8a755682a149d97f133576e95b3
binhex <megalith01@gmail.com>
2019-08-15 13:05:39 +0100

    reload tint2 on process end

---
+++ run/nobody/tint2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# run in infinite loop to restart process if it ends
+while true; do
+
+	tint2_running="false"
+
+	# check if tint2 is running
+	if ! pgrep -x "/usr/bin/tint2" > /dev/null; then
+
+			echo "[info] tint2 not running"
+
+	else
+
+			tint2_running="true"
+
+	fi
+
+	if [[ "${tint2_running}" == "false" ]];then
+
+			# run tint2 (creates task bar) with custom theme
+			/usr/bin/tint2 -c /home/nobody/tint2/theme/tint2rc
+
+	fi
+
+	sleep 10s
+
+done
