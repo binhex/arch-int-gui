bd5b1d183b83877b0be93703155262823b51b161
binhex <megalith01@gmail.com>
2019-11-17 22:21:19 +0000

    switch obmenu to aur

--- build/root/install.sh
+++ build/root/install.sh
@@ -33,7 +33,7 @@ fi
 ####
 
 # define aur packages
-aur_packages="websockify ttf-font-awesome novnc hsetroot"
+aur_packages="obmenu websockify ttf-font-awesome novnc hsetroot"
 
 # call aur install script (arch user repo)
 source aur.sh
