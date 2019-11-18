85aa9f3ad2fb5032fed08f986415150f97dc9d3a
binhex <megalith01@gmail.com>
2019-11-18 13:39:29 +0000

    path change for novnc, launch.sh removed

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -31,8 +31,8 @@ if [[ -n "${WEBPAGE_TITLE}" ]]; then
 	vncconfig -set desktop="${WEBPAGE_TITLE}"
 fi
 
-# starts novnc (web vnc client) - note the launch.sh also starts websockify to connect novnc to tigervnc server
-/usr/share/novnc/utils/launch.sh --listen 6080 --vnc localhost:5900 &
+# starts novnc (web vnc client) - note also starts websockify to connect novnc to tigervnc server
+/usr/sbin/websockify --web /usr/share/webapps/novnc/ 6080 localhost:5900 &
 
 # start dbus (required for libreoffice menus to be viewable when started via openbox right click menu) and launch openbox (window manager)
 dbus-launch openbox-session &
